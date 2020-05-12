5a28d5011009662b32923995999df0937a5d386c
Hau <nguyenhunghau@fabercompany.co.jp>
2020-05-12 13:37:38 +0700

    Init hibernate and spring

---
+++ src/main/java/com/assignment/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.assignment.service;
+
+import java.util.List;
+
+import com.assignment.entity.Customer;
+
+public interface CustomerService {
+
+    public List < Customer > getCustomers();
+
+    public void saveCustomer(Customer theCustomer);
+
+    public Customer getCustomer(int theId);
+
+    public void deleteCustomer(int theId);
+
+}
